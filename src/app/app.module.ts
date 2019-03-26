7b52cbe3f6d5cbb6c9932791897e442091a050b1
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-27 00:34:37 +0100

    create components and configure routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MovieListComponent} from './components/movie-list/movie-list.component';
+import {CharacterListComponent} from './components/character-list/character-list.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    CharacterListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'movies', component: MovieListComponent},
+      {path: 'characters', component: CharacterListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
